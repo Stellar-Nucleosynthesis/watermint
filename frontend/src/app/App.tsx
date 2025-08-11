e296245c305130472a110819b37f3914391e6a24
Stellar-Nucleosynthesis <stepanenkonazar356@gmail.com>
2025-08-11 11:02:15 +0300

    chore(frontend): add Mantine UI library

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from "./AppRoutes";
+import { MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
 
 function App(){
     return (
-        <BrowserRouter>
-            <AppRoutes/>
-        </BrowserRouter>
+        <MantineProvider>
+            <BrowserRouter>
+                <AppRoutes/>
+            </BrowserRouter>
+        </MantineProvider>
     );
 }
 
